9ff1322a4929c4f0f21b6c9b00bd9ffe266a26b3
Jorge Denys <jorgedenys@gmail.com>
2023-02-18 19:40:47 -0300

    Auditing data

--- backend/src/main/java/com/jdsjara/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jdsjara/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jdsjara.dscatalog.dto.CategoryDTO;
 import com.jdsjara.dscatalog.entities.Category;
 import com.jdsjara.dscatalog.repositories.CategoryRepository;
+import com.jdsjara.dscatalog.services.exceptions.DatabaseException;
 import com.jdsjara.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -58,6 +61,18 @@ public class CategoryService {
 			return new CategoryDTO(entity);	
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id não encontrado: " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);	
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id não encontrado: " + id);
+		} 
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de Integridade.");
 		}
 	}
 	
--- backend/target/classes/import.sql
+++ backend/target/classes/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category (name) values ('Books');
-INSERT INTO tb_category (name) values ('Electronics');
-INSERT INTO tb_category (name) values ('Computers');
+INSERT INTO tb_category (name, created_at) values ('Books', now());
+INSERT INTO tb_category (name, created_at) values ('Electronics', now());
+INSERT INTO tb_category (name, created_at) values ('Computers', now());
