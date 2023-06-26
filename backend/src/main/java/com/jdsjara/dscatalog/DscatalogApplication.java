4f3ecc5d4fdb085a5626c4f40fb0980d908624b3
Jorge Denys <jorgedenys@gmail.com>
2023-06-26 19:27:12 -0300

    Lidando com o problema N + 1 (ProductRepository)

--- backend/src/main/java/com/jdsjara/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/jdsjara/dscatalog/DscatalogApplication.java
@@ -2,10 +2,10 @@ package com.jdsjara.dscatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class DscatalogApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
 	}
